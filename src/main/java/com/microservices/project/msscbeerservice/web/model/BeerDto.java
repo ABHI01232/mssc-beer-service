59b64dc888f466bff539ed8843e26c3b3263ab5d
ABHI01232 <kaushik01232@gmail.com>
2021-12-28 22:12:18 +0530

    Added Validations closes #8

--- src/main/java/com/microservices/project/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/microservices/project/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -18,14 +22,33 @@ import java.util.UUID;
 public class BeerDto {
 
 
-    private UUID id;
-    private Integer version;
-    private OffsetDateTime createdDate;
-    private OffsetDateTime lastModifiedDate;
-    private String beerName;
-    private BeerStyleEnum beerStyle;
+ @Null
+ private UUID id;
+ @Null
+ private Integer version;
+
+  @Null
+  private OffsetDateTime createdDate;
+
+  @Null
+ private OffsetDateTime lastModifiedDate;
+
+
+   @NotBlank
+  private String beerName;
+
+   @NotNull
+   private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
+
     private Integer quantityOnHand;
 
 }
